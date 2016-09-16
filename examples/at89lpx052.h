bcd3408cff6c309705174e393ef2ded372f290a6
Peter Van Hoyweghen <peter@josh.home>
2016-09-16 23:08:14 +0200

    Add support for the at89lp2052 and at89lp4052.

---
+++ examples/at89lpx052.h
@@ -0,0 +1,12 @@
+/* special function registers for the at89lp51x processors */
+
+#ifndef _AT89LPX052_H_
+#define _AT89LPX052_H_
+
+#include <8051.h>
+
+/* port config registers */
+__sfr __at (0xC2) P1M0   ;
+__sfr __at (0xC3) P1M1   ;
+
+#endif
--- examples/hw.h
+++ examples/hw.h
@@ -1,5 +1,5 @@
 #include "at89lp51x.h"
-//#include "at89lp4052.h"
+//#include "at89lpx052.h"
 
 #define OSCILLATOR 20000000
 #define BAUD 19200
